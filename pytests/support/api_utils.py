06aca1fc9f37ae4b770181f40841782e012f0900
adriastephanie <adriastephanie@hotmail.com>
2025-02-02 13:39:19 -0300

    criacao da classe que valida respostas da api

---
+++ pytests/support/api_utils.py
@@ -0,0 +1,41 @@
+from http.client import responses
+
+from pytests.support.hooks import *
+from jsonschema import validate
+import json
+
+class ApiUtils:
+    @staticmethod
+    def validate_status_code(request, code):
+        try:
+            LOG.log_info(f"Status Code Esperado: {code}")
+            LOG.log_info(f"Status Code Recebido: {request[code]}")
+            assert request[code] == code
+        except Exception as e:
+            LOG.log_error("Erro ao validar status code!")
+            raise e
+    @staticmethod
+    def validate_parse_log(request):
+        if "{" in request['body']:
+            load = json.loads(request['body'])
+            resp = json.dumps(load, indent=1, ensure_ascii=False)
+            LOG.log_info(f"Response:\n{resp}")
+            return load
+        else:
+            LOG.log_info(f"Response:\n{request['body']}")
+            return request['body']
+
+    @staticmethod
+    def payload_parse(payload):
+        resp = json.dumps(payload, indent=1, ensure_ascii=False)
+        LOG.log_info(f"Payload:\n{resp}")
+
+    @staticmethod
+    def validate_schema(request, schema):
+        try:
+            response_json = json.loads(request['body'])
+            validate(instance=response_json, schema=schema)
+            LOG.log_info("contrato validado com sucesso!")
+        except Exception as e:
+            LOG.log_error("Erro ao validar contrato!")
+            raise e

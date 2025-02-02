0d5e9bc9296ee7b2fef44331d0f09c88293637ec
adriastephanie <adriastephanie@hotmail.com>
2025-02-02 14:37:38 -0300

    ajustando .env para receber BASE_URL

--- pytests/clients/post_livraria_client.py
+++ pytests/clients/post_livraria_client.py
@@ -1,15 +1,16 @@
 from playwright.sync_api import sync_playwright
 from pytests.support.api_utils import ApiUtils
 from pytests.support.hooks import *
+import os
 
 class PostLivrariaClient:
     @staticmethod
     def post_livros(payload):
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.post("http://132.145.174.237:3000/livros", data=payload)
+            response = context.post(f"{os.environ['BASE_URL']}", data=payload)
             LOG.log_info("POST")
-            LOG.log_info("URL: http://132.145.174.237:3000/livros")
+            LOG.log_info(f"URL: {os.environ['BASE_URL']}")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
--- pytests/support/hooks.py
+++ pytests/support/hooks.py
@@ -1,10 +1,12 @@
 import pytest
+from dotenv import load_dotenv
 from pytests.support.log_service import LogService
 LOG = LogService
 
 @pytest.fixture(scope="session", autouse=True)
 def before_all():
     LOG.log_info("Teste Log before all")
+    load_dotenv()
 
 @pytest.fixture(autouse=True)
 def before_after():

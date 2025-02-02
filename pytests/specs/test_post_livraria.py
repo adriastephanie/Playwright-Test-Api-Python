5c29dd8a7fe0a2b77e779ee87d77ec967ea393a9
adriastephanie <adriastephanie@hotmail.com>
2025-02-02 14:29:33 -0300

    validando contrato da api post

--- pytests/specs/test_post_livraria.py
+++ pytests/specs/test_post_livraria.py
@@ -1,8 +1,11 @@
 import pytest
+
+from pytests.schemas.post_livro_schema import post_schema
 from pytests.support.hooks import *
 from pytests.mocks.livraria_mock import *
 from pytests.clients.post_livraria_client import PostLivrariaClient
 from pytests.support.api_utils import ApiUtils
+from pytests.schemas import *
 
 @pytest.mark.crud_livros
 def test_post_livro():
@@ -10,3 +13,4 @@ def test_post_livro():
     ApiUtils.payload_parse(payload)
     response = PostLivrariaClient.post_livros(payload)
     PostLivrariaClient.validate_response(response, 201)
+    ApiUtils.validate_schema(response, post_schema)

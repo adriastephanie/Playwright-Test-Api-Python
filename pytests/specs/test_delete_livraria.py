b07b3027b27c60e827ee920d45113eef2e176395
adriastephanie <adriastephanie@hotmail.com>
2025-04-09 15:22:32 -0300

    add novos testes no delete

--- pytests/specs/test_delete_livraria.py
+++ pytests/specs/test_delete_livraria.py
@@ -1,3 +1,4 @@
+from pytests.examples.examples_test_delete_livraria import examples_delete_livraria_invalid_values
 from pytests.schemas.delete_livraria_schema import delete_schema
 from pytests.support.hooks import *
 from pytests.mocks.livraria_mock import *
@@ -17,3 +18,20 @@ def test_delete_livro():
     response = DeleteLivrariaClient.delete_livros(id)
     Commom.validate_response(response, 200)
     ApiUtils.validate_schema(response, delete_schema)
+
+
+
+# teste funcional passando id
+@pytest.mark.crud_livros
+def test_delete_id_livro():
+    response = DeleteLivrariaClient.delete_livros(2150)
+    Commom.validate_response(response, 200)
+    ApiUtils.validate_schema(response, delete_schema)
+
+# teste dos valores invalidos do get
+@pytest.mark.crud_livros
+@pytest.mark.parametrize("value, code", examples_delete_livraria_invalid_values)
+def test_delete_livro_invalid_values(value, code):
+    value_change = Commom.values_change(value)
+    response = DeleteLivrariaClient.delete_livros(value_change)
+    Commom.validate_response(response, code)
